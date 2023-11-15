9c3d19bd06f4e7c3dc4cc3b44661b924c7cc60e3
sefacakirrr <102422508+sefacakirrr@users.noreply.github.com>
2023-11-15 14:00:16 +0300

    test3

---
+++ src/pages/homePage.js
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+class HomePage extends Component{
+    //  https://randomuser.me/api/
+    
+  constructor(props){
+   super(props)
+   this.state = {
+     count:15,
+     checkSystem:false,
+   }
+   console.log("CONSTRUCTOR------>")
+  }
+    
+  //  static getDerivedStateFromProps(props,state){
+  //   console.log("getDerivedStateFromProps--------->")
+  //   if(props.name === state.text){
+  //         return {
+  //           checkSystem:true
+  //         }
+  //   }
+  //   return null
+  //  }
+
+  // shouldComponentUpdate(nextProps,nextState){
+  //   console.log("NEXT-PROPS---->",nextProps)
+  //    return nextProps.number < nextState.count
+  // }
+
+   
+ 
+  handleClick=()=>{
+    this.setState({
+      countTwo: this.state.count + 1
+    })
+  }
+
+   componentDidMount() {
+    console.log("didmount---------------->HELLO")
+   }
+   
+    render() {
+      window.onload = function(){
+        console.log("SAYFA TAMAMEN YÜKLENDİ")
+      }
+        console.log("RENDER-------->")
+        console.log("countTwo",this.state.count)
+        return (
+            <div> 
+                <b>LİFECYCLE METHODLARINI ÖĞRENİYORUZ</b>
+                <div>
+                  {
+                    this.state.count 
+                  }
+                </div>
+                <button onClick={this.handleClick}>Tıkla</button>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
